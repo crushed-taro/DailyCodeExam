3d57943c24e9fdf2e1164df7b35fcc98be80da6a
crushed-taro <97sinsang@gmail.com>
2024-12-21 22:22:15 +0900

    chore: 2024-12-21 일일 학습 내용 업로드

---
+++ src/main/java/CodingExam/BaekJoon/Exam24267.java
@@ -0,0 +1,20 @@
+package CodingExam.BaekJoon;
+
+import java.io.*;
+
+public class Exam24267 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        long n = Long.parseLong(br.readLine());
+
+        br.close();
+
+        bw.write((n*(n-1)*(n-2)/6)+"\n" + 3);
+        bw.flush();
+        bw.close();
+
+    }
+}
